1408a3202e4ce54df150b600cfd190a01c94b10b
braian <braianve7@gmail.com>
2025-05-02 10:43:51 -0300

    Agregando mas cosas al login y modificaciones en el navbar

--- FitManager/Controllers/HomeController.cs
+++ FitManager/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using FitManager.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FitManager.Controllers
@@ -17,7 +18,7 @@ namespace FitManager.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
